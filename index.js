01c5cdb553319da93138b8e1960c4ab69ac93c02
goldingj <jgolding.jgo@gmail.com>
2025-06-02 16:13:14 -0400

    created routes creating envelopes, retrieving all envelopes, and retrieving envelopes by name

--- index.js
+++ index.js
@@ -2,13 +2,52 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+const envelopes = [];
+const totalBudget = 0;
 
 
+/* Create new envelopes */
+app.post('/envelopes', (req, res, next) => {
+    const {name, amount} = req.body;
 
-app.get('/', (req, res, next) => {
-    res.send("Hello World");
+    if(!name || !amount){
+        return res.status(400).json({error: 'Must enter a name and an amount'});
+    }
+
+    const newEnvelope = {
+        id: envelopes.length + 1,
+        name,
+        amount
+    }
+
+    envelopes.push(newEnvelope);
+
+    res.status(201).json(newEnvelope);
 });
 
+/* Retrieve all envelopes*/
+app.get('/envelopes', (req, res, next) => {
+    res.json(envelopes);
+})
+
+
+/*Retrieve envelopes by name*/
+app.get('/envelopes/:name', (req, res, next) => {
+    const {name} = req.params;
+     if(!name){
+        return res.status(400).json({error: 'Must enter name of envelope to retrieve'});
+     }
+
+     const envelope = envelopes.find(env => env.name.toLowerCase() === name.toLowerCase());
+
+     if(!envelope){
+        return res.status(404).json({error: "No envelope by that name found"});
+     }
+
+     res.status(200).json(envelope);
+})
 
 
 
